61da6fda4fb82d6115db91e431252c6b6d948eb0
snowshow <9400049@gmail.com>
2011-12-03 19:47:40 +0100

    ***

--- settings.py
+++ settings.py
@@ -98,7 +98,8 @@ TEMPLATE_LOADERS = (
 
 TEMPLATE_CONTEXT_PROCESSORS = (
     # default template context processors
-    'django.core.context_processors.auth',
+ #   'django.core.context_processors.auth',  deoreciated
+    'django.contrib.auth.context_processors.auth',
     'django.core.context_processors.debug',
     'django.core.context_processors.i18n',
     'django.core.context_processors.media',
